5cf06067ecf53584fb00239844a96b22a4e3afdc
Nastase Ramon <nastase.ramon17@gmail.com>
2015-10-24 17:49:39 +0300

    Added - read from file support

--- Algorithms/C++/DFS.cpp
+++ Algorithms/C++/DFS.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
-#include <vector>
-
+#include <iostream> // cout
+#include <vector>  // vector
+#include <cstdio> // fprintf
+#include <cstdlib> // exit()
 using namespace std;
 
 class Graph
@@ -57,31 +58,65 @@ public:
 
 int main(int argc, char const *argv[])
 {
-	Graph *g = new Graph(7);
 
-	g->addEdge(0, 1);
-	g->addEdge(0, 2);
-	g->addEdge(0, 3);
+	bool usingFiles = false;
 
-
-	g->addEdge(1, 5);
-	g->addEdge(1, 6);
+	if(usingFiles == false) {
 
 
-	g->addEdge(2, 4);
+		Graph *g = new Graph(7);
+
+		g->addEdge(0, 1);
+		g->addEdge(0, 2);
+		g->addEdge(0, 3);
 
 
-	g->addEdge(3, 2);
-	g->addEdge(3, 4);
-
-	g->addEdge(4, 1);
+		g->addEdge(1, 5);
+		g->addEdge(1, 6);
 
 
-	g->addEdge(6, 4);
+		g->addEdge(2, 4);
 
 
-	g->DFS();
+		g->addEdge(3, 2);
+		g->addEdge(3, 4);
+
+		g->addEdge(4, 1);
 
 
+		g->addEdge(6, 4);
+
+
+		g->DFS();
+	}
+	else {
+
+		FILE *fIN;
+
+		if(argc != 2) {
+			fprintf(stderr, "Usage: ./DFS <fileName> \n");
+			exit(0);
+		}
+
+
+		if((fIN = fopen(argv[1],"r")) == NULL) {
+			fprintf(stderr, "Can't open file %s\n", argv[1]);
+			exit(0);
+		}
+
+		int N, M, x, y;
+		fscanf(fIN, "%d %d", &N, &M);
+
+
+		Graph *gp = new Graph(N);
+
+		for(int i = 0; i < M; ++i) {
+
+			fscanf(fIN, "%d %d", &x, &y);
+			gp->addEdge(x, y);
+		}
+
+		gp->DFS();
+	}
 	return 0;
 }
